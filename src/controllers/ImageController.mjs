c4716af29927864bfbbf80c4d0c416fd72116fce
Ben A <benjamin.anoufa@gmail.com>
2022-05-19 14:50:20 +0200

    remove test bd

--- src/controllers/ImageController.mjs
+++ src/controllers/ImageController.mjs
@@ -9,18 +9,22 @@ import RelationRealtyImageModel from '../models/RelationRealtyImage.mjs';
 class ImageController {
   addOne(req, res) {
     imageFormatter(req.files.image.data)
-    .then(image => {fs.writeFileSync(
-        path.join(__dirname, 'public', 'realty-images', image.name),
-        image.data);
-        return image;
-    })
-    .then(image => new ImageModel().addOne(image.name))
-    .then(result => {
-     // console.log(parseInt(req.body.destination), parseInt(result.insertId));
-      new RelationRealtyImageModel().addOne(parseInt(req.body.destination), parseInt(result.insertId));
-    }
-      )
-    .then(result => res.status(201).send(result));
+      .then(image => {
+        fs.writeFileSync(
+          path.join(__dirname, 'public', 'realty-images', image.name),
+          image.data
+        );
+        return image.name;
+      })
+      .then(imageName => new ImageModel().addOne(imageName))
+      .then(result => {
+        // console.log(parseInt(req.body.destination), parseInt(result.insertId));
+        new RelationRealtyImageModel().addOne(
+          parseInt(req.body.destination),
+          parseInt(result.insertId)
+        );
+      })
+      .then(result => res.status(201).send(result));
   }
 }
 
